6785078f05b88312024aab841051bb0635707d8d
sohail-285 <mdsohailmujawar07@gmail.com>
2025-03-29 10:29:04 +0530

    React
    ..

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <form>
+            <input type="text"/>
+            <button type="submit">submit</button>
+        </form>
+    );
+}
---
+++ Main.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+export default function counter(){
+    const [count,setCount]=useState(0);
+    return(
+        <div>
+            <button>count:{count}</button>
+        </div>
+    );
+}
